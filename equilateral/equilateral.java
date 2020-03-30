0aeaadc6c876ec24bba1f2145b9e6313c326106f
pavan kumar <pavankumar@Eeswars-MacBook-Air.local>
2020-03-30 17:48:29 +0200

    added java and nodejs code

--- equilateral/equilateral.java
+++ equilateral/equilateral.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner;
+public class equilateral {
+    public static void main(String args[]){
+    Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the  number of line to print: ");
+        int num = scan.nextInt();
+        scan.close();
+    }
+    public static void printPattern(int n){
+        
+        for(int i=1;i<=n;i++){
+            String spaces = "";
+            for(int j=n;j>i;j--){
+                spaces = spaces+" ";
+            }
+            System.out.println(spaces+""+i);
+        
+        }
+    }
+}
